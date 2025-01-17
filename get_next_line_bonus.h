7d45ed571d10eaed34556a20d8ec790d3cd023ce
Arda Yavaşgül <ardayvsgl@Arda-MacBook-Pro.local>
2025-01-17 22:32:38 +0300

    get_next_line %125

---
+++ get_next_line_bonus.h
@@ -0,0 +1,17 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_new_str_buf(char *str_buf);
+
+#endif
